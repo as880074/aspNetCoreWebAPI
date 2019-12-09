f6f2d8de79ce0c96219bbadf2f2c2347b1b01488
NickChi <as880074@gmail.com>
2019-12-09 21:28:18 +0800

    自動更新該欄位為更新當下的時間

---
+++ ContosoWebAPI/ContosoWebAPI/Models/EntiySaveChange.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoWebAPI.Models
+{
+    public partial class ContosouniversityContext
+    {
+        public override int SaveChanges()
+        {
+            var entities = this.ChangeTracker.Entries();
+            foreach (var entry in entities) 
+            {
+                if (entry.State == EntityState.Modified) 
+                {
+                    entry.CurrentValues.SetValues(new { DateModified = DateTime.Now });
+                }
+            }
+            return base.SaveChanges();
+        }
+    }
+}
